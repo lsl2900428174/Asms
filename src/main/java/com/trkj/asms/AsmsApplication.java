bfb3c81446dccc860b0199a77d4ece817772e6ba
lishilong0925 <2900428174@qq.com>
2021-07-11 00:33:18 +0800

    汽车服务管理系统后端搭建。

---
+++ src/main/java/com/trkj/asms/AsmsApplication.java
@@ -0,0 +1,13 @@
+package com.trkj.asms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsmsApplication.class, args);
+    }
+
+}
