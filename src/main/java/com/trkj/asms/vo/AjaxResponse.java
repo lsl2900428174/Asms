4303d819e4592347f6e777b59d440f86cb2dbec9
LAPTOP-H69LL4DV\86176 <2421074243@qq.com>
2021-07-12 10:00:00 +0800

    小宇

--- src/main/java/com/trkj/asms/vo/AjaxResponse.java
+++ src/main/java/com/trkj/asms/vo/AjaxResponse.java
@@ -1,9 +1,5 @@
 package com.trkj.asms.vo;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 6a75340af3fcb8de092b42fe5383ae29db6b200e
 import com.trkj.asms.exception.CustomError;
 import com.trkj.asms.exception.CustomErrorType;
 import lombok.Data;
@@ -36,13 +32,11 @@ public class AjaxResponse {
             resultBean.setMessage(e.getMessage());
         }else if(e.getCode() == CustomErrorType.SYSTEM_ERROR.getCode()){
             resultBean.setMessage(e.getMessage() + ",系统出现异常，请联系管理员!");
-<<<<<<< HEAD
-=======
+
         }else if(e.getCode()== CustomErrorType.PAGE_NOT_FOUND_ERROR.getCode()){
             resultBean.setMessage("未能找到所请求的资源！");
         }else if(e.getCode()== CustomErrorType.ACCOUNT_ERROR.getCode()){
             resultBean.setMessage(e.getMessage());
->>>>>>> 6a75340af3fcb8de092b42fe5383ae29db6b200e
         }else{
             resultBean.setMessage("系统出现未知异常，请联系管理员电话!");
         }
@@ -65,11 +59,7 @@ public class AjaxResponse {
      * @return AjaxResponse 封闭后的响应对象
      */
     public static AjaxResponse success(Object data) {
-<<<<<<< HEAD
-        AjaxResponse resultBean = new AjaxResponse();
-=======
        AjaxResponse resultBean = new AjaxResponse();
->>>>>>> 6a75340af3fcb8de092b42fe5383ae29db6b200e
         resultBean.setSuccess(true);
         resultBean.setCode(200);
         resultBean.setMessage("success");
