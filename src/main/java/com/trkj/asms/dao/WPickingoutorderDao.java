ab29c53a4b0ddce8ec9fd01650b24591c38c4a8c
Dreams <3191074903@qq.com>
2021-07-12 14:07:55 +0800

    合并之后正常启动 杨超 2021-7-12

--- src/main/java/com/trkj/asms/dao/WPickingoutorderDao.java
+++ src/main/java/com/trkj/asms/dao/WPickingoutorderDao.java
@@ -1,6 +1,7 @@
 package com.trkj.asms.dao;
 
 import com.trkj.asms.entity.WPickingoutorder;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.List;
  * @author makejava
  * @since 2021-07-12 13:29:18
  */
+@Mapper
 public interface WPickingoutorderDao {
 
     /**
